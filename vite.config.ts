8aa19b01b18677ec9071481beaf36db392ae9470
Artymiik <aworkprogramm@gmail.com>
2025-02-15 19:01:24 +0500

    changed bundle dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `[name].bundle.js`,
+				chunkFileNames: `[name].chunk.js`,
+				assetFileNames: `[name].[ext]`,
+			},
+		},
+	},
 });
