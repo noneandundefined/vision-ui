a4fd3c3336feb31c425aed96e39f42584398506e
Artymiik <aworkprogramm@gmail.com>
2025-02-21 13:22:51 +0500

    fix: work in huskty return default code

--- src/components/Effects.tsx
+++ src/components/Effects.tsx
@@ -1,7 +1,7 @@
 const Effects = () => {
 	return (
 		<>
-			<div className="w-[50rem] h-[50rem] rounded-[50%] bg-[#fff] fixed -top-[20rem] -left-[10rem] blur-[10rem] opacity-[0.065] -z-[1]"></div>
+			<div className="w-[50rem] h-[50rem] rounded-[50%] bg-[#fff] fixed -top-[20rem] -left-[10rem] blur-[10rem] opacity-[0.07] -z-[1]"></div>
 		</>
 	);
 };
