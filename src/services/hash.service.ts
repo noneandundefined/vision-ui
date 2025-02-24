f31cf457db24bea2a6ee70168b64f1db98e5ea4e
Artymiik <aworkprogramm@gmail.com>
2025-02-24 16:20:19 +0500

    added base hashing service class for password (#45)

---
+++ src/services/hash.service.ts
@@ -0,0 +1,26 @@
+class HashService {
+	private static hash(input: string): string {
+		let hash = 5381;
+		const len = input.length;
+
+		for (let i = 0; i < len; i++) {
+			hash = (hash << 5) + hash + input.charCodeAt(i);
+		}
+
+		hash = ((hash >>> 0) * 0x5bd1e995) | 0;
+		hash ^= hash >>> 15;
+		hash = ((hash >>> 0) * 0x5bd1e995) | 0;
+		hash ^= hash >>> 13;
+		hash = ((hash >>> 0) * 0x5bd1e995) | 0;
+		hash ^= hash >>> 16;
+
+
+		return Math.abs(hash).toString(36);
+	}
+
+	public static getHash(str: string): string {
+        return this.hash(str);
+	}
+}
+
+export default new HashService();
