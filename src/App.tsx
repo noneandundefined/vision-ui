5a2d831f348cdca6d781766c30a3ab69338ddb25
Artymiik <aworkprogramm@gmail.com>
2025-02-25 16:39:09 +0500

    added base-to-base authenticate (cookie (oauth)) (#48)

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,10 @@ const App = () => {
 							)}
 						</>
 					) : (
-						<Protected setIsError={setIsError} setResponseError={setResponseError} />
+						<Protected
+							setIsError={setIsError}
+							setResponseError={setResponseError}
+						/>
 					)}
 				</>
 			) : (
