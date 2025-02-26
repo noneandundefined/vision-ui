0d7a91d3edb338fe606d1c7ff4aa65c6c7a888aa
Artymiik <aworkprogramm@gmail.com>
2025-02-26 18:42:34 +0500

    added logo vision-ui to folder public/ (bg-none)

--- test/components/index-global.test.tsx
+++ test/components/index-global.test.tsx
@@ -44,21 +44,27 @@ describe('Global <Index /> testing...', () => {
 	it('Component <ServerLoad /> is active', async () => {
 		const { container } = render(
 			<MemoryRouter>
-				<Index data={{
-					system: { cpu_usage: 50, memory_usage: 70, network_recv: 200 },
-					database: {
-						database_queries: 100,
-						database_error: 5,
-						database_avg_latency_ms: 20,
-					},
-					requests: {
-						request_avg_latency_ms: 15,
-						request_count: 200,
-						request_success_count: 195,
-						request_error_count: 5,
-					},
-					last_errors: [],
-				}} />
+				<Index
+					data={{
+						system: {
+							cpu_usage: 50,
+							memory_usage: 70,
+							network_recv: 200,
+						},
+						database: {
+							database_queries: 100,
+							database_error: 5,
+							database_avg_latency_ms: 20,
+						},
+						requests: {
+							request_avg_latency_ms: 15,
+							request_count: 200,
+							request_success_count: 195,
+							request_error_count: 5,
+						},
+						last_errors: [],
+					}}
+				/>
 			</MemoryRouter>
 		);
 
@@ -68,26 +74,38 @@ describe('Global <Index /> testing...', () => {
 	it('Show base monitoring', async () => {
 		const { container } = render(
 			<MemoryRouter>
-				<Index data={{
-					system: { cpu_usage: 50, memory_usage: 70, network_recv: 200 },
-					database: {
-						database_queries: 100,
-						database_error: 5,
-						database_avg_latency_ms: 20,
-					},
-					requests: {
-						request_avg_latency_ms: 15,
-						request_count: 200,
-						request_success_count: 195,
-						request_error_count: 5,
-					},
-					last_errors: [],
-				}} />
+				<Index
+					data={{
+						system: {
+							cpu_usage: 50,
+							memory_usage: 70,
+							network_recv: 200,
+						},
+						database: {
+							database_queries: 100,
+							database_error: 5,
+							database_avg_latency_ms: 20,
+						},
+						requests: {
+							request_avg_latency_ms: 15,
+							request_count: 200,
+							request_success_count: 195,
+							request_error_count: 5,
+						},
+						last_errors: [],
+					}}
+				/>
 			</MemoryRouter>
 		);
 
-		expect(container.querySelector('.server__load__memory')?.textContent).toEqual('70%');
-		expect(container.querySelector('.server__load__cpu')?.textContent).toEqual('50.00%');
-		expect(container.querySelector('.server__load__network')?.textContent).toEqual('2.00%');
+		expect(
+			container.querySelector('.server__load__memory')?.textContent
+		).toEqual('70%');
+		expect(
+			container.querySelector('.server__load__cpu')?.textContent
+		).toEqual('50.00%');
+		expect(
+			container.querySelector('.server__load__network')?.textContent
+		).toEqual('2.00%');
 	});
 });
